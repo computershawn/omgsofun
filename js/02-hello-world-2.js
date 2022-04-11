ad199b3b061117d0a58522b99da38a7beaf1dc7a
computershawn <shontek@gmail.com>
2022-04-10 19:51:18 -0700

    Copied code from other repo
    aw yeah

---
+++ js/02-hello-world-2.js
@@ -0,0 +1,7 @@
+/* /js/02-hello-world-2.js */
+
+// When we get into more complex javascript, we will usually
+// have external javascript files like this one. 
+
+// alert() is a built-in function.
+alert("Hello, World!");
